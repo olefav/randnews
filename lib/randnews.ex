048c385968d420b8bab900360c03b1a5942cd77c
olefav <olefav@gmail.com>
2024-02-19 11:11:45 +0100

    Remove dead website.

--- lib/randnews.ex
+++ lib/randnews.ex
@@ -3,7 +3,7 @@ defmodule Randnews do
   Documentation for Randnews.
   """
 
-  @sites ["NV", "Pravda", "5", "112", "Censor"]
+  @sites ["NV", "Pravda", "5", "Censor"]
 
   def dump(file_path, pages_count, sites \\ @sites) do
     File.open(file_path, [:write, encoding: :utf8], fn file ->
