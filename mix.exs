57600e08a2763bff605f26a76cb3a0aa9d9ad803
olefav <olefav@gmail.com>
2024-02-28 10:13:04 +0100

    config_env/0 introduced in Elixir 1.11, adapting application requirements and CI config.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Randnews.MixProject do
     [
       app: :randnews,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
